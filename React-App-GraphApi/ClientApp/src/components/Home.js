77fd19a7f516829714af3197b52d886ce734ba44
Ahsan Raza <Ahsan.Raza@itjustfitts.com>
2019-10-18 16:57:49 +0100

    blog post demo updates

--- React-App-GraphApi/ClientApp/src/components/Home.js
+++ React-App-GraphApi/ClientApp/src/components/Home.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
+import {
+    Button
+} from 'reactstrap';
+import '../styles/profile-page.css';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    static renderUserProfile(props) {
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="profile-img">
+                        <img className="rounded-circle" src={props.photo} alt="" />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="profile-head">
+                        <h5>
+                            {props.user.displayName}
+                        </h5>
+                        <h6>
+                            {props.user.jobTitle}
+                        </h6>
+                    </div>
+                </div>
+                <div className="col-md-2">
+                    <Button color="primary" onClick={props.authButtonMethod}>Logout</Button>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        let contents = null;
+
+        if (this.props.isAuthenticated) {
+            contents = this.props.state.isLoading
+                ? <p className="text-center"><em>... Loading Your Profile ...</em></p>
+                : Home.renderUserProfile(this.props.state);
+        }
+
+        else {
+            contents =
+                <div>
+                    <h2>Welcome to React with Microsoft Graph</h2>
+                    <Button color="primary" onClick={this.props.authButtonMethod}>Click here to sign in</Button>
+                </div>;
+        }
+
+        return (
+            <div className="container user-profile">
+                {contents}
+            </div>
+        );
+    }
 }
